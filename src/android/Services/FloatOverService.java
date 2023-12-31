168d992e21fce5419fbdecbaf2c0337bc8f72df2
Gordon Johnson Abura Onono <83360844+Gordonjohnao@users.noreply.github.com>
2023-12-31 15:33:55 +0300

    Updated import android.graphics.Point;

--- src/android/Services/FloatOverService.java
+++ src/android/Services/FloatOverService.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.graphics.Point;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
