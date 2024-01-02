a81401cf6500911f2ed91d149bd9aa60ccec9b52
Gordon Johnson Abura Onono <83360844+Gordonjohnao@users.noreply.github.com>
2024-01-03 02:24:47 +0300

    Update FloatOver.java

--- src/android/FloatOver.java
+++ src/android/FloatOver.java
@@ -23,61 +23,61 @@ import org.apache.cordova.floatOver.Services.ServiceParameters;
 
 
 public class FloatOver extends CordovaPlugin {
-		private static final String TAG = "FloatOver";
-	  private static final String ACTION_OPEN_OVER_APP_VIEW = "open";
-		private static final String ACTION_CLOSE_OVER_APP_VIEW = "close";
-		private static final String ACTION_CHECK_OVER_APP_PERMISSION = "checkPermission";
-	  private static final String FOLDER_PATH = "file:///android_asset/www/";
-		public Activity activity;
-		public ServiceParameters serviceParameters;
-		CallbackContext callback;
-		public FloatOver() {
-			super();
+	private static final String TAG = "FloatOver";
+	private static final String ACTION_OPEN_OVER_APP_VIEW = "open";
+	private static final String ACTION_CLOSE_OVER_APP_VIEW = "close";
+	private static final String ACTION_CHECK_OVER_APP_PERMISSION = "checkPermission";
+	private static final String FOLDER_PATH = "file:///android_asset/www/";
+	public Activity activity;
+	public ServiceParameters serviceParameters;
+	CallbackContext callback;
+	public FloatOver() {
+		super();
+	}
+
+	@Override
+	public boolean execute(String action, JSONArray options,
+	final CallbackContext callbackContext) throws JSONException {
+		activity = this.cordova.getActivity();
+		serviceParameters = new ServiceParameters(activity);
+
+		if (action.equals(ACTION_CHECK_OVER_APP_PERMISSION)) {
+
+		if (Build.VERSION.SDK_INT >= 23) {
+				if (!checkDrawOverAppsPermission(activity)) {
+						Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,Uri.parse("package:" + activity.getPackageName()));
+						activity.startActivityForResult(intent, 0);
+						callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Granting Draw Over Apps Permission "));
+				}
+				else
+				{
+						callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Permission already Granted "));
+				}
+
+		}
+	    return true;
 		}
 
-		@Override
-		public boolean execute(String action, JSONArray options,
-				final CallbackContext callbackContext) throws JSONException {
-					activity = this.cordova.getActivity();
-					serviceParameters = new ServiceParameters(activity);
+	     else if (action.equals(ACTION_OPEN_OVER_APP_VIEW)) {
 
-					if (action.equals(ACTION_CHECK_OVER_APP_PERMISSION)) {
+		if (checkDrawOverAppsPermission(activity)) {
+			if (options.getJSONObject(0).has("path")) {
+				openServiceWithHTMLfile(callbackContext,options.getJSONObject(0) );
+				callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Draw Over Apps Should Start Successfully "));
+			}
 
-								if (Build.VERSION.SDK_INT >= 23) {
-										if (!checkDrawOverAppsPermission(activity)) {
-												Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,Uri.parse("package:" + activity.getPackageName()));
-												activity.startActivityForResult(intent, 0);
-												callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Granting Draw Over Apps Permission "));
-										}
-										else
-										{
-												callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Permission already Granted "));
-										}
+		}
+		else
+		{
+				callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "No Permission Granted for Draw Over Apps"));
+		}
 
-								}
-		            return true;
-	        		}
+		return true;
+			}
+		else if (action.equals(ACTION_CLOSE_OVER_APP_VIEW)){
 
-					else if (action.equals(ACTION_OPEN_OVER_APP_VIEW)) {
-
-								if (checkDrawOverAppsPermission(activity)) {
-									if (options.getJSONObject(0).has("path")) {
-										openServiceWithHTMLfile(callbackContext,options.getJSONObject(0) );
-										callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Draw Over Apps Should Start Successfully "));
-									}
-
-								}
-								else
-								{
-										callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "No Permission Granted for Draw Over Apps"));
-								}
-
-								return true;
-					}
-					else if (action.equals(ACTION_CLOSE_OVER_APP_VIEW)){
-
-						 activity.stopService(new Intent(activity, FloatOverService.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-					}
+			 activity.stopService(new Intent(activity, FloatOverService.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+		}
 
         	return false;
 
