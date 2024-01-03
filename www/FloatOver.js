0731e8573fc08e33b1ebadfbd5086d5e1a4d5738
Gordon Johnson Abura Onono <83360844+Gordonjohnao@users.noreply.github.com>
2024-01-03 04:05:25 +0300

    Update FloatOver.js

--- www/FloatOver.js
+++ www/FloatOver.js
@@ -8,8 +8,20 @@ module.exports = {
     startOverApp: function (options,successCallback, failureCallback) {
         cordova.exec(successCallback, failureCallback, 'FloatOver', 'open', [ options ]);
     },
+   // closeOverApp: function () {
+        //cordova.exec(nooperation, nooperation, 'FloatOver', 'close', []);
+    //}
     closeOverApp: function () {
-        cordova.exec(nooperation, nooperation, 'FloatOver', 'close', []);
-    }
+    cordova.exec(
+        function() {
+            console.log('Plugin closed successfully.');
+        },
+        function(error) {
+            console.error('Error while closing plugin:', error);
+        },
+        'FloatOver', 'close', []
+    );
+}
+
    
 };
