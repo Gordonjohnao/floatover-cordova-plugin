91e195b7b15c9e34cc8bc5e69a62700353b1dd7a
Gordon Johnson Abura Onono <83360844+Gordonjohnao@users.noreply.github.com>
2024-01-06 03:41:29 +0300

    Update FloatOver.java

--- src/android/FloatOver.java
+++ src/android/FloatOver.java
@@ -76,7 +76,11 @@ public class FloatOver extends CordovaPlugin {
 		return true;
 			}
 		else if (action.equals(ACTION_CLOSE_OVER_APP_VIEW)){
-			 activity.stopService(new Intent(activity, FloatOverService.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+			    //Intent stopServiceIntent = new Intent(activity, FloatOverService.class);
+			    //stopServiceIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			    //stopServiceIntent.setAction(FloatOverService.ACTION_STOP_SERVICE);
+			    //activity.stopService(stopServiceIntent);
+		  activity.stopService(new Intent(activity, FloatOverService.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
 		}
 
         	return false;
